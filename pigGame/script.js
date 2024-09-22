ae531b60570721a9c1956f4c22f7b48e1c7b6d25
rezaazimibar <www.beta04@gmail.com>
2024-09-22 19:23:36 +0330

    initialized counter project

--- pigGame/script.js
+++ pigGame/script.js
@@ -58,7 +58,7 @@ holdBtn.addEventListener('click', function () {
     const currentMainScore = document.querySelector(`#score--${activePlayer}`);
     currentMainScore.textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
