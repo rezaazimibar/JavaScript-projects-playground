05592a931461e3f7101694d6fbc2c211cef0d991
rezaazimibar <www.beta04@gmail.com>
2024-10-09 13:21:05 +0330

    custom Video Player current time and video time logic

--- customVideoPlayer/script.js
+++ customVideoPlayer/script.js
@@ -8,8 +8,13 @@ let forwardButton = controls.querySelector(".forward");
 let volumeButton = controls.querySelector(".volume");
 let fullscreenButton = controls.querySelector(".fullscreen");
 
+let timeArea = document.querySelector(".timer");
+let videoCurrentTime = timeArea.querySelector(".currentTime");
+let videoTime = timeArea.querySelector(".videoTime");
+
 //play video
 playButton.addEventListener("click", function () {
+  videoTime.textContent = getTime(media.duration);
   if (media.paused) {
     togglePlayButton();
     media.play();
@@ -17,6 +22,10 @@ playButton.addEventListener("click", function () {
     togglePlayButton();
     media.pause();
   }
+});
+
+media.addEventListener("timeupdate", function () {
+  videoCurrentTime.textContent = getTime(media.currentTime);
 });
 
 //-5 second rewind
@@ -36,4 +45,25 @@ function togglePlayButton() {
   let icon = playButton.querySelector("i");
   icon.classList.toggle("ion-md-pause");
   icon.classList.toggle("ion-md-play");
+}
+
+//get the current time and show
+function getTime(time) {
+  let minutes = Math.floor(time / 60);
+  let second = Math.floor(time - minutes * 60);
+
+  let minuteValue;
+  let secondValue;
+
+  if (minutes < 10) {
+    minuteValue = `0${minutes}`;
+  } else {
+    minuteValue = minutes;
+  }
+  if (second < 10) {
+    secondValue = `0${second}`;
+  } else {
+    secondValue = second;
+  }
+  return `${minuteValue} : ${secondValue}`;
 }
