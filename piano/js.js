8b19d34ad4de6368f9394b3e2c84cbefb5407059
rezaazimibar <www.beta04@gmail.com>
2024-10-16 18:10:15 +0330

    piano porject clicking style logic

--- piano/js.js
+++ piano/js.js
@@ -1,10 +1,20 @@
 const runningNote = document.querySelector(".nowplaying");
+const allKeys = document.querySelectorAll(".key");
 
 window.addEventListener("keydown", function (e) {
-  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if (!key) return;
-  
+
   const keyNote = key.getAttribute("data-note");
+  key.classList.add("playing");
   runningNote.innerHTML = keyNote;
 });
+
+allKeys.forEach((key) =>
+  key.addEventListener("transitionend", removeTransition)
+);
+
+function removeTransition() {
+  this.classList.remove("playing");
+}
