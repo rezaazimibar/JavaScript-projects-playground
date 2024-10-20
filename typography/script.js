de3ba9e3e540cf78d3833a3bad70ec20b21e6117
rezaazimibar <www.beta04@gmail.com>
2024-10-20 19:15:49 +0330

    typography project shadow movment logic

--- typography/script.js
+++ typography/script.js
@@ -8,5 +8,14 @@ hero.addEventListener("mousemove", function (event) {
   let { offsetX: x, offsetY: y } = event;
   x += event.target.offsetLeft;
   y += event.target.offsetTop;
-  console.log(x,y)
+
+  const xWalk = Math.round((x / width) * boxSize - boxSize / 2);
+  const yWalk = Math.round((y / height) * boxSize - boxSize / 2);
+
+  txt.style.textShadow = `
+  ${xWalk}px ${yWalk *-.7}px 0 rgba(238 ,82,83,0.7),
+  ${xWalk * -1}px ${yWalk *.2}px 0 rgba(52 ,31 ,151 ,.7),
+  ${yWalk}px ${xWalk * -1.5}px 0 rgba(243 ,104 ,224 ,.7),
+  ${yWalk * -1.2}px ${xWalk}px 0 rgba(254 ,202 ,87 ,.7)
+  `;
 });
