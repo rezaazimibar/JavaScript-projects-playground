458ed8e055ae4de0621b8a541862ac170c36984a
rezaazimibar <www.beta04@gmail.com>
2024-10-21 19:38:28 +0330

    initialized drag and drop list project

---
+++ dragAndDropList/listAPI.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    id: 1,
+    title: "1st item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 2,
+    title: "2sd item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 3,
+    title: "3rd item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 4,
+    title: "4th item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 5,
+    title: "5th item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 6,
+    title: "7th item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+  {
+    id: 7,
+    title: "6th item",
+    Text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+  },
+];
