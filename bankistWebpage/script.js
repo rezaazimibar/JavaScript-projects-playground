c99ac3ed2cc2372abfb8b0f167b6d92a69b8707a
rezaazimibar <www.beta04@gmail.com>
2025-03-26 13:03:59 +0330

    bankitWebpage: impelement logic behind scroll smoothly to a specific section after click a button

--- bankistWebpage/script.js
+++ bankistWebpage/script.js
@@ -21,7 +21,6 @@ const closeModal = function () {
 
 btnsOpenModal.forEach(char => char.addEventListener('click', openModal));
 
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -30,3 +29,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  // window.scrollTo(s1coords.left + window.pageXOffset, 858)
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+//section_1
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'we are not happy we are miserable <button class="btn btn--close-cookie">Do it !</button>';
+
+// const header = document.querySelector('.header');
+// header.append(message);
+// header.prepend(message);
+
+// console.log(getComputedStyle(message).boxSizing);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+// console.log(document.documentElement);
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt)
+
+// console.log(logo.getAttribute('alt'))
+// logo.setAttribute('company', 'here it is')
+// console.log(logo.classList)
