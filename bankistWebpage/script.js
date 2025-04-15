6b83df2458b1884cf679b8f38672ff72872c612a
rezaazimibar <www.beta04@gmail.com>
2025-04-15 12:12:19 +0330

    bankitWebpage: add an effect to the sections as they become visible

--- bankistWebpage/script.js
+++ bankistWebpage/script.js
@@ -128,23 +128,40 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // observer.observe(section1);
 
 const header = document.querySelector('.header');
-const navHeight = nav.getBoundingClientRect().height
+const navHeight = nav.getBoundingClientRect().height;
 
-const stickyNav = function(entries){
-  const[entry] = entries;
-  console.log(entry)
-  if (!entry.isIntersecting) nav.classList.add('sticky')
-  else nav.classList.remove('sticky')
-}
-
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px`
+  rootMargin: `-${navHeight}px`,
 });
-headerObserver.observe(header)
+headerObserver.observe(header);
 
+//-----Revile-sections-----
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.3,
+});
+
+allSection.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 ///////////////////////////////////////////////////
 ///////////////////////////////////////////////////
 ///////////////////////////////////////////////////
