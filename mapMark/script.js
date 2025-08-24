da9a3f0bf686f62dc354bd645e6a42f9165fb902
rezaazimibar <www.beta04@gmail.com>
2025-08-24 21:14:53 +0330

    mapMark: Implement data storage class (Workout, Running, Cycling)

--- mapMark/script.js
+++ mapMark/script.js
@@ -11,6 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration); 
+    this.cadence = cadence
+    this.calcPace()
+  }
+  calcPace(){
+    // min/km
+    this.pace = this.duration / this.distance
+    return this.pace
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain
+    this.calcSpeed()
+  }
+
+  calcSpeed(){
+    // km/h
+    this.speed = this.distance / (this.duration / 60)
+    return this.speed
+  }
+
+}
+// const run = new Running([39, 40], 4.2, 100, 170)
+// const cycling = new Cycling([39, 60], 10, 120, 540)
+// console.log(run, cycling)
+
+
+////////////////////////////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 
 class App {
   #map;
