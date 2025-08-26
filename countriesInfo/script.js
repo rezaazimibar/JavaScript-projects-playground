997485e3191f0816c22d256b32eeccdf7044eb1f
rezaazimibar <www.beta04@gmail.com>
2025-08-26 19:46:36 +0330

    contriesInfo: get contries data from api and display it on DOM

--- countriesInfo/script.js
+++ countriesInfo/script.js
@@ -5,6 +5,42 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-console.log('first block')
-setTimeout(()=>{console.log("at time")}, 5000)
-console.log('last block')
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/name/${country}`
+    );
+    request.send();
+  
+    request.addEventListener('load', function () {
+      const [, data] = JSON.parse(this.responseText);
+      console.log(data);
+      const html = `
+      <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>`;
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+      countriesContainer.style.opacity = 1;
+    });
+  };
+  
+  
+  // console.log('first block')
+  // setTimeout(()=>{console.log("at time")}, 5000)
+  // console.log('last block')
+  // https://countries-api-836d.onrender.com/countries/
+  
+  getCountryData('iran')
+  getCountryData('united state')
+  getCountryData('pt')
+  getCountryData('in')
