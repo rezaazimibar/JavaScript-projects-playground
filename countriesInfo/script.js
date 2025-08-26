cec2c7ab5bb1afa801fe5724d184b8aafdb33821
rezaazimibar <www.beta04@gmail.com>
2025-08-26 14:12:29 +0330

    contries info: project initialization

---
+++ countriesInfo/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+console.log('first block')
+setTimeout(()=>{console.log("at time")}, 5000)
+console.log('last block')
